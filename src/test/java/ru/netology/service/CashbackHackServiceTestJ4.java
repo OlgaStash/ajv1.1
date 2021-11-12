da785fbf6c3a5f69550259b45355338b1ae4578c
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-11-12 12:10:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJ4.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTestJ4 {
+
+    @Test
+    public void shouldTestRemainFrom0() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 0;
+
+        int actual = service.remain(payment);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestRemainFrom1() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1;
+
+        int actual = service.remain(payment);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestRemainFrom1000() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1000;
+
+        int actual = service.remain(payment);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTestNG {
+
+    @Test
+    public void shouldTestRemainFrom0() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 0;
+
+        int actual = service.remain(payment);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldTestRemainFrom1() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1;
+
+        int actual = service.remain(payment);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldTestRemainFrom1000() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1000;
+
+        int actual = service.remain(payment);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+}
